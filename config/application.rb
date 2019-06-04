94fd1da5a1ecb957f5d8ab314ff4d258e4343aac
Riccardo <ricky.pagliuca@gmail.com>
2019-06-04 16:18:00 +0200

    first commit

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Progettolab
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.serve_static_assets = true
 
     # Don't generate system test files.
     config.generators.system_tests = nil
